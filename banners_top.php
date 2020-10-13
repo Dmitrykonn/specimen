772c64e02634b82a65f16854227f26ee9a8854f2
Dmitrykonn <48092146+Dmitrykonn@users.noreply.github.com>
2020-10-13 17:14:44 +0500

    Add files via upload

---
+++ banners_top.php
@@ -0,0 +1,8 @@
+$ban[1] = '<a href="" target="_blank"><img src="https://img1.picmix.com/output/stamp/normal/6/8/7/1/1281786_cb440.gif;"/></a>';
+$ban[2] = '<a href="" target="_blank"><img src="https://img1.picmix.com/output/stamp/normal/8/6/4/3/333468_6175a.gif;"/></a>';
+$ban[3] = '<a href="" target="_blank"><img src="https://img1.picmix.com/output/stamp/normal/6/8/7/1/1281786_cb440.gif;"/></a>';
+$ban[4] = '<a href="" target="_blank"><img src="https://img1.picmix.com/output/stamp/normal/4/0/1/9/259104_1b7be.gif;"/></a>';
+$ban[5] = '<a href="" target="_blank"><img src="https://img1.picmix.com/output/stamp/normal/6/8/7/1/1281786_cb440.gif;"/></a>';
+
+$ran = rand(1,5);
+echo $ban[$ran];
